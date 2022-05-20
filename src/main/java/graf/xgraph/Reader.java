dfbb2a6430741321bef0fa06eb91f08f828b7dc3
Dawid Cybulski <01169548@pw.edu.pl>
2022-05-20 11:44:48 +0200

    From file to ArrayList

--- src/main/java/graf/xgraph/Reader.java
+++ src/main/java/graf/xgraph/Reader.java
@@ -2,12 +2,29 @@ package graf.xgraph;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Reader {
     Scanner sc;
+    private int readedRow;
+    private int readedColumn;
+    private ArrayList<Line> readedLines = new ArrayList<Line>();
+
+    public int getReadedColumn() {
+        return readedColumn;
+    }
+
+    public int getReadedRow() {
+        return readedRow;
+    }
+
+    public ArrayList<Line> getReadedLines() {
+        return readedLines;
+    }
+
     public Reader(String pathName) throws IOException {
         sc = new Scanner(new java.io.File(pathName), StandardCharsets.UTF_8);
     }
@@ -18,10 +35,13 @@ public class Reader {
             scanFirstLine(sc.nextLine());
         }
         System.out.println("Array");
+        int currentLine = 0;
         while (sc.hasNextLine()) {
             // System.out.println(sc.nextLine());
-            scanNextLine(sc.nextLine());
+            scanNextLine(sc.nextLine(), currentLine);
+            currentLine++;
         }
+        System.out.println(readedRow + " " + readedColumn);
     }
 
     public void scanFirstLine(String s) {
@@ -39,8 +59,10 @@ public class Reader {
         }
         System.out.println(first + " " + second);
         System.out.println(first+second);
+        readedRow = first;
+        readedColumn = second;
     }
-    public void scanNextLine(String s) {
+    public void scanNextLine(String s,int line) {
         Pattern pattern2 = Pattern.compile("(\\d+)\\s:\\s+(\\d+.\\d+)\\s");
         Pattern pattern3 = Pattern.compile("(\\d+)\\s:\\s+(\\d+.\\d+)\\s+(\\d+)\\s:\\s+(\\d+.\\d+)\\s");
         Matcher matcher2 = pattern2.matcher(s);
@@ -55,6 +77,7 @@ public class Reader {
         if (matcher2.matches()) {
             first = Integer.parseInt(matcher2.group(1));
             second = Double.parseDouble(matcher2.group(2));
+            readedLines.add(new Line(line, first,second));
         }
         int third = 0;
         double fourth = 0.0;
@@ -63,6 +86,8 @@ public class Reader {
             second = Double.parseDouble(matcher3.group(2));
             third = Integer.parseInt(matcher3.group(3));
             fourth = Double.parseDouble(matcher3.group(4));
+            readedLines.add(new Line(line, first,second));
+            readedLines.add(new Line(line, third,fourth));
         }
         System.out.println(first + " " + second + " " + third + " " + fourth);
     }
